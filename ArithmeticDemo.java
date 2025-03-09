49945f7e894766bc3fc20e40fc736778bbcc3599
Remi Jay R. Concepcion <concepcionremijay_bsit@plmun.edu.ph>
2025-03-09 22:17:03 +0800

    Create ArithmeticDemo.java

---
+++ ArithmeticDemo.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ArithmeticDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input two numbers
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        // Perform arithmetic operations
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+
+        // Step-by-step calculation of the expression (num1 + num2) * 2 - num2 / 2
+        int expressionResult = (num1 + num2) * 2 - num2 / 2;
+
+        // Display the results
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+        System.out.println("Expression Result: " + expressionResult);
+        
+        scanner.close();
+    }
+}
